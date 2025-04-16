27f03f25fb8bb844dd65659f0e3621f07631d7f8
JarodTodd <74596618+JarodTodd@users.noreply.github.com>
2025-04-16 10:45:49 +0200

    Add Ironpython and DLS GUI scripts

---
+++ GUI.py
@@ -0,0 +1,323 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+import sys
+import subprocess
+import random
+
+ironpython_executable = r"C:\Users\jarod\IronPython3.4\ipy.exe"
+script_path = r"C:\Users\jarod\OneDrive\Documents\School\Jaar 3\Bachelorproject\IronPythonDLS.py"
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Delayline GUI")
+
+        # Central widget
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        # Layouts
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
+        hbox = QHBoxLayout()
+
+        initialize_button = QPushButton("Initialize")
+        initialize_button.clicked.connect(self.Initialize)
+        hbox.addWidget(initialize_button)
+
+        disable_button = QPushButton("Disable/Ready")
+        disable_button.clicked.connect(self.Disable_click)
+        hbox.addWidget(disable_button)
+
+        move_neg_button = QPushButton("Move -100ps")
+        move_neg_button.clicked.connect(self.Move_back_click)
+        hbox.addWidget(move_neg_button)
+
+        move_pos_button = QPushButton("Move +100ps")
+        move_pos_button.clicked.connect(self.Move_click)
+        hbox.addWidget(move_pos_button)
+
+        layout.addLayout(hbox)
+
+        hbox2 = QHBoxLayout()
+        self.delay_input = QLineEdit()
+        self.delay_input.setPlaceholderText("Enter delay time")
+        self.delay_input.setValidator(QDoubleValidator(-8672000.0, 8672000.0, 20, self))
+        self.delay_input.validator().setLocale(QLocale(QLocale.C))
+        self.delay_input.returnPressed.connect(self.Submitted)
+
+        self.delay_unit = QComboBox()
+        self.delay_unit.addItems(["ns", "ps", "fs"])
+        self.delay_unit.currentIndexChanged.connect(self.LabelChange)
+
+        hbox2.addWidget(self.delay_input)
+        hbox2.addWidget(self.delay_unit)
+
+        hbox3 = QHBoxLayout()
+
+        self.delay_label = QLabel("Delay (ns):", self)
+        hbox3.addWidget(self.delay_label)
+
+        self.delay_bar = QProgressBar()
+        self.delay_bar.setMinimum(0)
+        self.delay_bar.setMaximum(8672)  # max picoseconds delay
+        self.delay_bar.setValue(0)
+        self.delay_bar.setFormat(f"/8672")
+
+        hbox3.addWidget(self.delay_bar)
+
+        hbox4 = QHBoxLayout()
+
+        setref_button = QPushButton("Set Reference")
+        setref_button.clicked.connect(self.SetReference)
+        hbox4.addWidget(setref_button)
+
+        gotoref_button = QPushButton("Go to Reference")
+        gotoref_button.clicked.connect(self.GoToReference)
+        hbox4.addWidget(gotoref_button)
+
+
+        hbox5 = QHBoxLayout()
+
+        # Add file uploader
+        file_upload_button = QPushButton("Upload File")
+        file_upload_button.clicked.connect(self.showFileDialog)
+
+        vbox = QVBoxLayout()
+        runscript_button = QPushButton("Run Script")
+        runscript_button.clicked.connect(lambda: self.RunContent(self.text_display.toPlainText(), "forward"))
+
+        runscript_backwards_button = QPushButton("Run Script Backwards")
+        runscript_backwards_button.clicked.connect(lambda: self.RunContent(self.text_display.toPlainText(), "backwards"))
+
+        runscript_random_button = QPushButton("Run Script Random")
+        runscript_random_button.clicked.connect(lambda: self.RunContent(self.text_display.toPlainText(), "random"))
+
+        vbox.addWidget(runscript_button)
+        vbox.addWidget(runscript_backwards_button)
+        vbox.addWidget(runscript_random_button)
+        hbox5.addLayout(vbox)
+        hbox5.addWidget(file_upload_button)
+
+
+        self.file_label = QLabel("No file selected", self)
+        hbox5.addWidget(self.file_label)
+
+        self.text_display = QTextEdit()
+        self.text_display.setReadOnly(True)
+        hbox5.addWidget(self.text_display)
+
+        layout.addLayout(hbox2)
+        layout.addLayout(hbox3)
+        layout.addLayout(hbox4)
+        layout.addLayout(hbox5)
+
+    def run_script(self, argument):
+        try:
+            result = subprocess.Popen(
+                [ironpython_executable, script_path, argument],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            stdout, stderr = result.communicate()
+            print(stdout)
+            movement = ["Disable", "SetReference", "GetReference", "GetPosition"]
+            if argument not in movement:
+                stdout, stderr = result.communicate()
+                print(self.delay_bar.value())
+
+                # Decode the output
+                decoded_output = stdout.decode('utf-8')
+
+                # Strip unwanted characters and convert to float
+                clean_output = decoded_output.strip()
+                numerical_value = float(clean_output)
+
+                # Update the delay progress bar with the numerical value
+                self.update_delay_bar(numerical_value)
+                print(self.delay_bar.value())
+        except Exception as e:
+            print(f"Error executing IronPython script: {e}")
+        return stdout
+
+    def Initialize(self):
+        self.run_script("Initialize")
+
+    def Disable_click(self):
+        self.run_script("Disable")
+
+    def Move_click(self):
+        self.run_script("MovePositive")
+
+    def Move_back_click(self):
+        self.run_script("MoveNegative")
+
+    def SetReference(self):
+        self.run_script("SetReference")
+
+    def GoToReference(self):
+        self.run_script("GoToReference")
+
+    
+
+    def LabelChange(self):
+        selected = self.delay_unit.currentText()
+        if selected == "ns":
+            self.delay_label.setText("Delay (ns):")
+        elif selected == "ps":
+            self.delay_label.setText("Delay (ps):")
+        elif selected == "fs":
+            self.delay_label.setText("Delay (fs):")
+
+    def Submitted(self):
+        try:
+            unit = self.delay_unit.currentText()
+            value = float(self.delay_input.text())
+            barvalue = self.delay_bar.value()
+
+            if unit == "ns":
+                barvalue_ns = self.delay_bar.value() / 1000
+                if 0 <= value <= (8.672 - barvalue_ns) or (value < 0 and abs(value) <= barvalue_ns):
+                    self.run_script(f"MoveRelative {value}")
+                    self.update_delay_bar(barvalue + value*1000)
+                else:
+                    raise ValueError(f"Value is out of range.")
+            elif unit == "ps":
+                if 0 <= value <= (8672 - barvalue) or (value < 0 and abs(value) <= barvalue):
+                    self.run_script(f"MoveRelative {value/1000}")
+                    self.update_delay_bar(barvalue + value)
+                else:
+                    raise ValueError(f"Value is out of range.")
+            elif unit == "fs":
+                barvalue_fs = self.delay_bar.value()*1000
+                if 0 <= value <= (8672000 - barvalue_fs) or (value < 0 and abs(value) <= barvalue_fs):
+                    self.run_script(f"MoveRelative {value/1000000}")
+                    self.update_delay_bar(barvalue + value/1000)
+                else:
+                    raise ValueError(f"Value is out of range.")
+        except ValueError as ve:
+            self.show_error_message(str(ve))
+        except Exception as e:
+            self.show_error_message(f"An unexpected error occurred: {str(e)}")
+
+    def update_delay_bar(self, value):
+        value = max(0, min(value, self.delay_bar.maximum()))  # Ensure value is within the range
+        self.delay_bar.setValue(round(value, 3))
+        self.delay_bar.setFormat(f"{int(value)}/8672")
+
+    def show_error_message(self, error_message):
+        msgbox = QMessageBox()
+        msgbox.setWindowTitle("Error")
+        msgbox.setText("An error occurred:")
+        msgbox.setInformativeText(error_message)
+        msgbox.setIcon(QMessageBox.Critical)
+        msgbox.setStandardButtons(QMessageBox.Ok)
+        msgbox.exec()
+
+    def showFileDialog(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Select a .txt File", "", "Text Files (*.txt);;All Files (*)")
+        if fileName:
+            self.file_label.setText(fileName)
+            try:
+                with open(fileName, 'r') as file:
+                    content = file.read()
+                
+                self.text_display.setText(content)
+            except Exception as e:
+                self.show_error_message(f"Failed to load file: {e}")
+        return content
+    
+    def RunContent(self, content, orientation):
+        # Split the content into lines
+        lines = content.splitlines()
+        parsed_content = []
+
+        
+
+
+        for line in lines:
+            # Split each line by commas
+            items = line.split(",")
+            for item in items:
+                item = item.strip()  # Remove any surrounding whitespace
+                if item:  # Ensure the item is not empty
+                    letters = ""
+                    numbers = ""
+                    for char in item:
+                        if char.isdigit() or char == "." or char == "-":  # Include '-' for negative numbers
+                            numbers += char  # Collect numeric characters, decimal points, and negative signs
+                        else:
+                            letters += char  # Collect alphabetic characters
+                    if numbers:  # Check if numbers exist and convert to float
+                        try:
+                            parsed_content.append((letters.strip(), float(numbers)))
+                        except ValueError:
+                            # Handle invalid numbers gracefully
+                            parsed_content.append((letters.strip(), None))
+                    else:
+                        parsed_content.append((letters.strip(), None))  # If no numbers, append None
+
+
+        if parsed_content == []:
+            self.show_error_message("No file was uploaded.")
+            return
+
+        if orientation == 'backwards':
+            parsed_content.reverse()
+
+        if orientation == 'random':
+            random.shuffle(parsed_content)
+
+        ref = self.run_script("GetReference")
+        position = self.run_script("GetPosition")
+
+        ref = float(ref.decode('utf-8').strip())
+        position = float(position.decode('utf-8').strip())
+
+        for item in parsed_content:
+            if ref + item[1] < 0:
+                self.show_error_message(f"Reference point is out of range.{item[1]}")
+                return
+            if ref + item[1] > 8672:
+                self.show_error_message(f"Reference point is out of range.{item[1]}")
+                return
+
+        if round(ref, 3) != round(position, 3):
+            self.run_script("GoToReference")
+            self.update_delay_bar(ref*1000)
+
+        last_item = 0
+        for item in parsed_content:
+            barvalue = self.delay_bar.value()
+            pos = item[1]
+            pos = pos - last_item # Adjust value to be centered at reference point (t_0)
+            print(f"pos: {pos}, last_item: {last_item}, item: {item}, barvalue: {barvalue}")
+
+            if item[0] == 'ns' or item[0] == 'nanosecond' or item[0] == 'nanoseconds':
+                self.run_script(f"MoveRelative {pos}")
+                self.update_delay_bar(barvalue + pos*1000)
+            elif item[0] == 'ps' or item[0] == 'picosecond' or item[0] == 'picoseconds':
+                self.run_script(f"MoveRelative {pos/1000}")
+                self.update_delay_bar(barvalue + pos)
+            elif item[0] == 'fs' or item[0] == 'femtosecond' or item[0] == 'femtoseconds':
+                self.run_script(f"MoveRelative {pos/1000000}")
+                self.update_delay_bar(barvalue + pos/1000)
+            last_item = item[1]
+
+        return parsed_content
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    result = subprocess.Popen(
+        [ironpython_executable, script_path, "GetPosition"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    stdout, _ = result.communicate()
+    window.update_delay_bar(float(stdout.decode('utf-8').strip())*1000)
+    sys.exit(app.exec())
---
+++ IronPythonDLS.py
@@ -0,0 +1,254 @@
+import sys
+
+import clr
+
+sys.path.append(
+    r"C:\Windows\Microsoft.NET\assembly\GAC_64\Newport.DLS.CommandInterface\v4.0_1.0.1.0__90ac4f829985d2bf")
+clr.AddReference("Newport.DLS.CommandInterface.dll")
+
+import System
+from CommandInterfaceDLS import *
+
+instrument = "COM5"  # Change this to the port the DLS is connected to
+myDLS = DLS()
+result = myDLS.OpenInstrument(instrument)
+c = 299792458  # Speed of light in m/s
+""" 
+    If you need to add a function to the document, you can look at the DL Controller Users Manual
+    or look uncomment the dir(myDLS) command 
+"""
+# print(dir(myDLS))
+# if result == 0:
+#     print('Open port COM5 => Succesful')
+
+# This uses a function from CommandInterfaceDLS to find the version
+
+"""
+This function is run on startup.
+It initializes the controller and homes it, putting it in the ready state.
+"""
+errorcode = myDLS.TS()[2]
+state = myDLS.TS()[3]
+
+
+def StartUp():
+    state = myDLS.TS()[3]
+    # Check if there is an error; if not, check the state. If it is not initialized --> initialize
+    if errorcode == "00000":
+        if state == "0A" or state == "0B" or state == "0D":
+            myDLS.IE()  # Initializing
+            myDLS.OR()  # Homing
+            position = myDLS.PA_Get()
+            state = state
+            return state
+        if state == "28":
+            myDLS.OR()
+            state = state
+            return state
+    if errorcode != "00000":
+        return Error(errorcode)
+
+
+def MoveAbsolute(delay):
+    state = myDLS.TS()[3]
+    if state == "46" or state == "47" or state == "48" or state == "49":
+        position = (delay * 10**-6) * c / 8
+        myDLS.PA_Set(position)  # Move to absolute position
+
+    else:
+        print("The controller is not in the right state to move.")
+        state = state
+        return print(myDLS.TE())
+
+    print("The position is now:", myDLS.PA_Get()[1], "mm")
+    state = state
+    return state
+
+
+def MoveRelative(delay):
+    state = myDLS.TS()[3]
+    if state == "46" or state == "47" or state == "48" or state == "49":
+        position = (delay * 10**-6) * c / 8
+        myDLS.PR_Set(position)
+
+    else:
+        print("The controller is not in the right state to move.")
+        state = state
+        return print(myDLS.TE())
+
+    position = myDLS.PA_Get()[1] * 1000000 * 8 / c
+    print(position)
+    state = state
+    return state, position
+
+
+"""
+This function puts the controller in the disable state if in ready state and vice versa.
+"""
+
+
+def DisableReady():
+    state = myDLS.TS()[3]
+    if state == "46" or state == "47" or state == "48" or state == "49":
+        myDLS.MM_Set(0)  # Disable the machine
+        print("The machine is now disabled.")
+    if state == "50" or state == "51" or state == "52":
+        myDLS.MM_Set(1)  # Ready the machine
+        print("The machine is now ready.")
+
+    if errorcode != "00000":
+        return Error(errorcode)
+    state = state
+    return state
+
+def SetReference():
+    state = myDLS.TS()[3]
+    position = myDLS.PA_Get()[1]
+    myDLS.RF_Set(position) # Set the reference position
+    state = state
+    return state
+
+def GetReference():
+    reference = myDLS.RF_Get()[1] * 1000000 * 8 / c
+    print(reference)
+    return reference
+
+def GoToReference():
+    state = myDLS.TS()[3]
+    reference = myDLS.RF_Get()[1]
+    myDLS.PA_Set(reference)  # Move to reference position
+    position = myDLS.PA_Get()[1] * 1000000 * 8 / c
+    print(position)
+    state = state
+    return state, position
+
+def GetPosition():
+    state = myDLS.TS()[3]
+    position = myDLS.PA_Get()[1] * 1000000 * 8 / c
+    print(position)
+    state = state
+    return state, position
+
+""" 
+Both SetAcceleration and SetVelocity functions work for the same states.
+The difference between the states is that if the value is set while in configuration mode, 
+the value is saved in the controller. If the value is set while in disabled or ready state,
+the saved value is only saved until the controller is turned off.
+"""
+
+def SetAcceleration(acceleration):
+    if state == "14":  # configuration mode
+        myDLS.AC_Set(acceleration)  # Set acceleration
+        print("The acceleration is now:", myDLS.AC_Get())
+
+    possible_states = ["46", "47", "48", "49", "50", "51", "52"]
+    if state in possible_states:  # disabled or ready state
+        myDLS.AC_Set(acceleration)
+        print("The acceleration is now:", myDLS.AC_Get())
+
+
+def SetVelocity(velocity):
+    if state == "14":  # configuration mode
+        myDLS.VA_Set(velocity)  # Set velocity
+        print("The velocity is now:", myDLS.VA_Get())
+
+    possible_states = ["46", "47", "48", "49", "50", "51", "52"]
+    if state in possible_states:  # disabled or ready state
+        myDLS.VA_Set(velocity)
+        print("The velocity is now:", myDLS.VA_Get())
+
+
+"""
+Put the controller in configuration mode or get it out of configuration mode
+"""
+
+
+def ConfigMode():
+    possible_states = ["0A", "0B", "0C", "0D", "0E", "0F", "10", "11", "12", "13"]
+    if state in possible_states:
+        myDLS.PW(1)  # Set the controller in configuration mode
+        print("The controller is now in configuration mode.")
+        state = state
+        return state
+    if state == "14":
+        myDLS.PW(0)
+        print("The controller is now in not initialized state")
+        state = state
+        return state
+    if state not in possible_states:
+        print(
+            "The controller is not in the not initialized state. Cannot enter configuration mode."
+        )
+        errorcode = myDLS.TS()[2]
+        return Error(errorcode)
+
+
+"""
+This function tells the error code 
+and translates the corresponding code to human language.
+"""
+
+
+def Error(errorCode):
+    if errorCode == "00001":
+        print("Bit end of run negative: Move carriage away from end/check cables")
+    elif errorCode == "00002":
+        print("Bit end of run positive: Move carriage away from end/check cables")
+    elif errorCode == "00004":
+        print(
+            "The current set by the <<QIL>> has been exceeded: Check carriage freedom"
+        )
+    elif errorCode == "00008":
+        print(
+            "The current set by the <<QIR>> has been exceeded: Check carriage freedom"
+        )
+    elif errorCode == "00010":
+        print(
+            "Controller internal fuse (CMS) broken or glitch power supply: Cycle Power ON/OFF/ON"
+        )
+    elif errorCode == "00020":
+        print(
+            "Close loop (PID) or load (FMP (weight)) parameters not optimized; something interfers with the displacement: Check payload weight. Check carriage freedom. Restore Factory Settings."
+        )
+    elif errorCode == "00040":
+        print(
+            "Home search sequence time exceed time defined with <<OT>> parameter: Check that Acceleration (AC) is not too low. Restore Factory Settings."
+        )
+
+    return
+
+
+if myDLS.TS()[2] != "00000":
+    code = myDLS.TS()[2]
+    Error(code)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        if command == "Initialize":
+            StartUp()
+        elif command == "MovePositive":
+            MoveRelative(0.1)  # Adjust value if needed
+        elif command == "MoveNegative":
+            MoveRelative(-0.1)
+        elif command == "Disable":
+            DisableReady()
+        elif command.startswith("MoveRelative"):
+            value = float(command.split()[1])
+            MoveRelative(value)
+        elif command == "SetReference":
+            SetReference()
+        elif command == "GoToReference":
+            GoToReference()
+        elif command == "GetPosition":
+            GetPosition()
+        elif command == "GetReference":
+            GetReference()
+        else:
+            print(f"Unknown command: {command}")
+    else:
+        print("No command provided.")
+
+# This shuts down the controller
+myDLS.CloseInstrument()
