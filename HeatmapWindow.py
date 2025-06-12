a3bca1e8c6ba2274a50283deea8dac449c6bd41e
Sebastiaan van Daalen <bassievdaalen@gmail.com>
2025-06-12 14:59:50 +0200

    Update HeatmapWindow.py

--- HeatmapWindow.py
+++ HeatmapWindow.py
@@ -27,7 +27,7 @@ class HeatmapWindow(QWidget):
         self.grid_layout = QGridLayout()
 
         # TAPlotWidget 
-        delay_times   = np.array([0.0, 0.2, 0.5, 1.0])
+        delay_times   = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]) # demo delays for initial empty draw
         pixel_indices = np.arange(1023)
         self.ta_widgets = TAPlotWidget(delay_times, pixel_indices) 
         self.ta_widgets.canvas_heatmap.setBackground('w')
